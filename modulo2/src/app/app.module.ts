0460b4d90beb1822b4715ca3abb7dba31120d6ce
Cristian Vargas <77356085+crispro1987@users.noreply.github.com>
2022-06-18 11:09:36 -0400

    servicio ejemplo

--- modulo2/src/app/app.module.ts
+++ modulo2/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { Pagina2Component } from './pagina2/pagina2.component';
 import { Pagina1Component } from './pagina1/pagina1.component';
+import { Componente1Component } from './componente1/componente1.component';
+import { Componente2Component } from './componente2/componente2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     Pagina2Component,
-    Pagina1Component
+    Pagina1Component,
+    Componente1Component,
+    Componente2Component
   ],
   imports: [
     BrowserModule,
--- modulo2/src/app/shared/contador.service.ts
+++ modulo2/src/app/shared/contador.service.ts
@@ -3,7 +3,19 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
+//esto es una clase
 export class ContadorService {
 
   constructor() { }
+
+  //Crear Variable
+
+  public contador: number = 0;
+
+  //Crear Metodo o funcion
+
+  incrementar(){
+    this.contador++;
+  }
 }
