e3b3692e34c0c64d1ca97ac3c02963e73b50593c
Cristian Vargas <77356085+crispro1987@users.noreply.github.com>
2022-06-18 10:10:09 -0400

    creacion otros componentes

--- modulo2/src/app/app.module.ts
+++ modulo2/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { Pagina2Component } from './pagina2/pagina2.component';
+import { Pagina1Component } from './pagina1/pagina1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    Pagina2Component,
+    Pagina1Component
   ],
   imports: [
     BrowserModule,
