7ba06e69712481c8d530982151be50e599906ded
Cristian Vargas <77356085+crispro1987@users.noreply.github.com>
2022-06-18 10:13:06 -0400

    creacion de rutas

--- modulo2/src/app/app-routing.module.ts
+++ modulo2/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Pagina1Component } from './pagina1/pagina1.component';
+import { Pagina2Component } from './pagina2/pagina2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/home", pathMatch: "full" },
+  { path: "/home", component: HomeComponent },
+  { path: "/pagina1", component: Pagina1Component },
+  { path: "/pagina2", component: Pagina2Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
